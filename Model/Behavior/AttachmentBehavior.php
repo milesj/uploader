d0b8b0478867c0df29924cb862d43e915271b685
Miles Johnson <mileswjohnson@gmail.com>
2013-11-24 18:17:15 -0800

    Fix function nesting order

--- Model/Behavior/AttachmentBehavior.php
+++ Model/Behavior/AttachmentBehavior.php
@@ -332,9 +332,9 @@ class AttachmentBehavior extends ModelBehavior {
                     $data[$attachment['dbColumn']] = $this->_renameAndMove($model, $transit->getOriginalFile(), $attachment);
 
                     // Fetch exif data before transforming
-                    $metaData = array_flip(array_filter(array_flip($transit->getOriginalFile()->toArray(), function($key) {
+                    $metaData = array_flip(array_filter(array_flip($transit->getOriginalFile()->toArray()), function($key) {
                         return (substr($key, 0, 4) === 'exif');
-                    })));
+                    }));
 
                     // Transform the files and save their path
                     if ($attachment['transforms']) {
